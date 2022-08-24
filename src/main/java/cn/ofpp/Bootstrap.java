c4b106df2f678fa690f8db8e5fcf5c5d3afd869c
hzl <hzl@qq.com>
2022-08-24 09:45:00 +0800

    'bug'

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx5f09e7a3119fc163";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "c0a67513f3ceda8d3ac7309ab52bf3a0";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "axiW5NPn27WrHj6sz7vO8HPPvBejzfGEeg6Ll36UatA";
 
     /**
      * 初始化
